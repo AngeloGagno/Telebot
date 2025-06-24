d8bc366b0d80cdda17f6bb0dbdee2c445dc548c7
AngeloGagno <angagelogagno@gmail.com>
2025-06-24 00:06:43 -0300

    refactor:alterando textos do bot

--- src/main.py
+++ src/main.py
@@ -10,21 +10,17 @@ class BotTele:
     def __init__(self):
         load_dotenv(override=True)
         token = os.environ['TOKEN']
-        self.bot = telebot.TeleBot(token)
-        
-        # Instancia AdminPanel com o mesmo bot e prepara o DB de chat_ids
-        self.admin_panel = AdminPanel(self.bot)
+        self.bot = telebot.TeleBot(token, parse_mode='HTML') 
 
-        # Instancia ContentTypes onde contem os tratadores de mensagem
+        # Instâncias das outras classes
+        self.admin_panel = AdminPanel(self.bot)
         self.content = ContentTypes(self.bot)
-    
-        # Instancia de ação do grupo 
         self.group = Controller()
-        # Registrar os handlers de comandos, broadcast e eventos de membros
+
+        # Registrar todos os handlers
         self.register_handlers()
 
     def register_handlers(self):
-        """Faz o tratamento dos comandos e eventos do bot"""
         bot_self = self 
 
         # /start
@@ -33,27 +29,28 @@ class BotTele:
             if bot_self.admin_panel.chat_ids_table.contains(where('chat_id') == message.chat.id):
                 bot_self.bot.reply_to(
                     message,
-                        "<b>Hello</b>, you have been accepted to <b>Hermod</b>. Now it's up to you: "
-    "send <b>10 files within 30 minutes</b> to continue here.<br><br>"
-    "<b>Sending files is mandatory and cannot be skipped.</b> "
-    "Your activity is monitored by us, so if you want to stay here, "
-    "<b>keep sending files constantly</b>.<br><br>"
-    "Welcome and <i>have a good time!</i>"
+                    "<b>Hello</b>, you have been accepted to <b>Hermod</b>.<br><br>"
+                    "Now it's up to you: send <b>10 files within 30 minutes</b> to continue here.<br><br>"
+                    "<b>Sending files is mandatory and cannot be skipped.</b><br>"
+                    "Your activity is monitored by us, so if you want to stay here, "
+                    "<b>keep sending files constantly</b>.<br><br>"
+                    "Welcome and <i>have a good time!</i>."
                 )
             else:
                 bot_self.bot.reply_to(
                     message,
-                     f'Hello <b>{message.from_user.first_name}</b>, welcome to <b>HERMOD</b>, '
-        'your secure way to send data. If you want to access our bot, press '
-        '<b>/join</b> to send your entry request and become part of this universe. '
-        '<i>Keep calm and be patient</i>.'
+                    f"Hello <b>{message.from_user.first_name}</b>, welcome to <b>HERMOD</b>.<br><br>"
+                    "Your secure way to send data.<br><br>"
+                    "If you want to access our bot, press <b>/join</b> to send your entry request and become part of this universe.<br><br>"
+                    "<i>Keep calm and be patient.</i>"
                 )
-                
+
+        # /join
+        @self.bot.message_handler(commands=['join'])
         def group_link(message):
             self.bot.reply_to(
                 message,
-                '[Press here to join](https://t.me/+Ki1GXFSSPzs1OGJh)',
-                parse_mode='Markdown'
+                '<a href="https://t.me/+Ki1GXFSSPzs1OGJh"> Press here to join the group</a>'
             )
 
         # /admin
@@ -68,11 +65,11 @@ class BotTele:
                 bot_self.admin_panel.add_admin(message, message.from_user.first_name)
                 return
             if not bot_self.admin_panel.db_admin.contains(where('chat_id') == message.chat.id):
-                bot_self.bot.reply_to(message, 'Access Denied. Only admins can use this command.')
+                bot_self.bot.reply_to(message, ' Access Denied. Only admins can use this command.')
             else:
                 bot_self.admin_panel.add_admin(message, message.from_user.first_name)
 
-        # Broadcast de textos, fotos e vídeos
+        # Broadcast de texto, foto e vídeo
         @self.bot.message_handler(content_types=['photo', 'video', 'text'])
         def broadcast(message):
             if bot_self.admin_panel.chat_ids_table.contains(where('chat_id') == message.chat.id):
@@ -80,17 +77,17 @@ class BotTele:
                 for chat in chat_ids:
                     if message.text:
                         self.content.text_sender(chat, message)
-                    if message.video:
+                    elif message.video:
                         self.content.video_sender(chat, message)
-                    if message.photo:
+                    elif message.photo:
                         self.content.photo_sender(chat, message)
-                        
-        # Quando alguém for adicionado ao grupo
+
+        # Novo membro no grupo
         @self.bot.message_handler(content_types=['new_chat_members'])
         def on_user_added(message):
-            self.group.add_user(message)            
+            self.group.add_user(message)
 
-        # Quando alguém sair ou for removido do grupo
+        # Membro saiu
         @self.bot.message_handler(content_types=['left_chat_member'])
         def on_user_removed(message):
             user = message.left_chat_member
@@ -102,4 +99,3 @@ class BotTele:
 if __name__ == '__main__':
     bot = BotTele()
     bot.run()
-
